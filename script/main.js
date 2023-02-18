232b76fde134fdeded27b9ad9419a0056df7a2d4
romeroc1996m <camilaromero.9603@gmail.com>
2023-02-18 17:05:56 -0500

    Adding attempts
    Adding attempts without solution

--- script/main.js
+++ script/main.js
@@ -4,7 +4,19 @@
 
 let navButtons = document.querySelectorAll('#buttonHolder img'), 
 	theHeadline = document.querySelector('#headLine h1'),
-	puzzleBoard = document.querySelector('#puzzle-board');
+	// collect the graggable pieces in the drag zone
+	puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+	// puzzlePieces1 = document.querySelectorAll('.puzzle-pieces1 img'),
+	// collectt ALL of the drop zone elements
+	dropZones = document.querySelectorAll('.drop-zone'),
+	puzzleBoard = document.querySelector('.puzzle-board'),
+	tempLink = document.querySelector('a'),
+	dropElement = document.querySelector("#dropTarget"),
+	// overlapThreshold = ('.puzzle-board'), 
+
+	// set up a global variable to store a reference to the dragged piece
+	// i need to know this lalter when i drop it on a zone
+	draggPiece;
 
 
 // functions go in the middle
@@ -13,18 +25,71 @@ let navButtons = document.querySelectorAll('#buttonHolder img'),
 // this is a 1 to many connection to elements in the DOM
 // the variable name is the "Basket"
 function changeBGImage() {
-debugger;
+
+	let newBGPath = "images/backGround" + this.id + ".jpg"; 
+
+//url('../images/backGround0.jpg')
+// debugger;
 // object.property = new value
-
 // change the backgorung image in the drop zone
-
 // theHeadline.textContent = "Drag and Drop is fun!"
 
-puzzleBoard.style.backgroundImage = 'url("../"images/backGround" + this.id + ".jpg")' ;
+	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`; 
 }
+
+function handleStartDrag() { 
+	console.log('started draggin a piece!', this); 
+	draggPiece = this;
+}
+
+function handleDragOver(e) { 
+	e.preventDefault();
+	console.log('dragging over me!')
+	//e.dataTransfer.setData("text", ev.target.id);
+}
+
+function handleDrop(e) { 
+	console.log('dropped on me!'); 
+	e.target.appendChild(document.getElementById(draggPiece));
+	//e.dataTransfer.getData("text");
+	
+
+
+	// dropZones = this;
+	
+	// if(draggPiece = overlapThreshold){
+	//	result = 'droppded on me!'}
+	// e.target.appendChild(document.getElementById(draggPiece))
+}
+
 
 // event handling at the bottom => how things react when you 
 // how is the user going to interact with the elements / controls you provide?
 // process a collection of elements and add an event handler to each
 
 navButtons.forEach(button => button.addEventListener('click', changeBGImage));
+
+// add the drag start handler to all of the puzzles 
+
+puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handleStartDrag));
+// add the dragover handling the 
+
+dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
+dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
+
+function blockDefaultBehaviour(e) { // e is shorthand for event -> in this case the nav event
+	//don't let the default behaviour of certain elements happen - block it
+	e.preventDefault();
+}
+
+// temp handling
+tempLink.addEventListener('click', blockDefaultBehaviour);
+
+// gragging limit
+dropElement.addEventListener("dragenter", (event) => {
+	event.preventDefault();
+  });
+  
+  dropElement.addEventListener("dragover", (event) => {
+	event.preventDefault();
+  });
